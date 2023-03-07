8cc0bc6e754d5c732e47eb5c01af7aaafb64eca0
do hyun <ehgusrlaeh@ucware.net>
2023-03-07 18:34:43 +0900

    03/07

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -7,18 +7,44 @@ const helloStrangerElement = getElementById('hello_stranger');
 const chattingBoxElement = getElementById('chatting_box');
 const formElement = getElementById('chat_form');
 
+//* global socket handler
+socket.on('user_connected', (data) => {
+    console.log(`user_connected: ${data}`)
+});
+
+//* event callback handler
+ const handleSubmit = (event) => {
+    event.preventDefault();
+    const inputValue = event.target.elements[0].value;
+    if(!inputValue) return
+    socket.emit('submit_chat',inputValue);
+    // 화면에다 그리기
+ }
+
+//* draw functions
+const drawHelloStranger = (username) => {
+    helloStrangerElement.innerHTML = `Hello, ${username} Stranger!`;
+};
+const drawNewChat = (message) => {
+    const wrapperChatBox = document.createElement('div');
+    const chatBox = `
+        <div>
+            ${message}
+        </div>`
+    wrapperChatBox.innerHTML = chatBox;
+    chattingBoxElement.appendChild(wrapperChatBox);
+}
+
 function helloUser() {
-  const username = prompt('What is your name?');
-  socket.emit('new_user', username, (data) => {
-    console.log(data);
-    });
-  socket.on('hello_user', (data) => {
-    console.log(data);
-  });
+//   const username = prompt('What is your name?');
+//   socket.emit('new_user', username, (data) => {
+//     drawHelloStranger(data);
+//     });
 }
 
 function init() {
   helloUser();
+  formElement.addEventListener('submit', handleSubmit);
 }
 
 init();
--- src/chats/chats.gateway.ts
+++ src/chats/chats.gateway.ts
@@ -1,31 +1,30 @@
 import { Logger } from '@nestjs/common/services';
 import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
 import { ConnectedSocket, MessageBody } from '@nestjs/websockets/decorators';
-import { OnGatewayInit } from '@nestjs/websockets/interfaces';
-import { Socket } from 'dgram';
+import { OnGatewayConnection, OnGatewayDisconnect, OnGatewayInit } from '@nestjs/websockets/interfaces';
+import { Socket } from 'socket.io';
 
 @WebSocketGateway({
     namespace: 'chattings',
 })
-export class ChatsGateway implements OnGatewayInit{
-    private logger= new Logger('chat')
+export class ChatsGateway implements OnGatewayInit,OnGatewayConnection,OnGatewayDisconnect{
+    private logger= new Logger('chat') 
     constructor() {
         this.logger.log('constructer!');
     }
     afterInit() {
-        this.logger.log('Initialized!');
+        this.logger.log('init!');
     }
     handleConnection(@ConnectedSocket() client: Socket) {
-        this.logger.log('Client connected: ' + client);
+        this.logger.log('Client connected: ' + client.id + 'namespace: ' + client.nsp.name);
     }
     handleDisconnect(@ConnectedSocket() client: Socket) {
-    this.logger.log('Client disconnected: ' + client);
+        this.logger.log('Client disconnected: ' + client.id + 'namespace: ' + client.nsp.name);
     }
   @SubscribeMessage('new_user')
   handleNewUser(@MessageBody() username : string, @ConnectedSocket() socket:Socket) {
-    console.log(username);
-    console.log(socket.address);
-    socket.emit('hello_user', 'Hello ' + username + '!');
-    return 'Hello World!';
+    // username을 db에 적재
+    socket.broadcast.emit('user_connected', username);
+    return username;
   }
 }
